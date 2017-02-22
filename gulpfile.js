217f5eeb7f816180a2de92b15db6fac159e2e0e9
xcatliu <xcatliu@gmail.com>
2017-02-22 10:26:20 +0800

    Add test html

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ const autoprefixer = require('autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const insert = require('gulp-insert');
 const pkg = require('./package.json');
+const tap = require('gulp-tap');
+const md = require('markdown-it')().use(require('markdown-it-anchor'));
 
 const postcssConfig = [autoprefixer({ browsers: [
   'last 5 iOS versions',
@@ -29,6 +31,8 @@ const postcssConfig = [autoprefixer({ browsers: [
 
 const SRC_DIR = path.resolve(__dirname, 'src');
 const DIST_DIR = path.resolve(__dirname, 'dist');
+const DEMO_SRC_DIR = path.resolve(__dirname, 'public');
+const DEMO_DIST_DIR = path.resolve(__dirname, 'public');
 
 gulp.task('default', ['build'], () => {
   gulp.watch([
@@ -63,3 +67,29 @@ gulp.task('build:mobi', () => gulp.src(`${SRC_DIR}/mobi.scss`)
   .pipe(insert.prepend(`/* Mobi.css v${pkg.version} ${pkg.homepage} */\n`))
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest(DIST_DIR)));
+
+gulp.task('build:demo', () => gulp.src(`${DEMO_SRC_DIR}/**/*.md`)
+  .pipe(tap(file => {
+    /* eslint no-param-reassign:0 */
+    file.contents = Buffer.concat([
+      new Buffer(`
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta http-equiv="x-ua-compatible" content="ie=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0,
+              maximum-scale=1.0, user-scalable=no"/>
+
+            <link rel="stylesheet" href="css/mobi.min.css" />
+          </head>
+          <body>
+      `),
+      new Buffer(md.render(file.contents.toString())),
+      new Buffer(`
+        </body>
+          </html>
+      `),
+    ]);
+  }))
+  .pipe(gulp.dest(DEMO_DIST_DIR)));
